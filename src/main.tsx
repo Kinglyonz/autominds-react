4b83f5377b2ceb12823ca4865ade212296ff6177
Autominds <team@autominds.org>
2026-01-05 10:52:39 -0500

    chore: Add ErrorBoundary to diagnose production crash

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import App from './App.tsx'
 import DemosPage from './pages/DemosPage.tsx'
 import CloudIDEPage from './pages/CloudIDEPage.tsx'
 import CodeReviewPage from './pages/CodeReviewPage.tsx'
+import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/demos" element={<DemosPage />} />
-        <Route path="/ide" element={<CloudIDEPage />} />
-        <Route path="/code-review" element={<CodeReviewPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/demos" element={<DemosPage />} />
+          <Route path="/ide" element={<CloudIDEPage />} />
+          <Route path="/code-review" element={<CodeReviewPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   </StrictMode>,
 )
 
